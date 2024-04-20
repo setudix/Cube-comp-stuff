dc7cfbbc8fb95c767db49b1560b107dce0ae0a6f
setu <setu@setu.com>
2024-04-21 01:58:47 +0600

    taking the name of the pdf file as command line arg

--- pdf2png-for-social-media/script.py
+++ pdf2png-for-social-media/script.py
@@ -1,10 +1,19 @@
+import sys
 from pdf2image import convert_from_path
 
 def pdf_to_png(pdf_path, output_path):
     images = convert_from_path(pdf_path)
-
     if images:
         images[0].save(output_path, 'PNG')
 
-pdf_to_png('early bird opening post.pdf', 'output.png')
-print("converted\n")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <pdf_file_path>")
+        sys.exit(1)
+
+    pdf_file_path = sys.argv[1]
+    pdf_file_name = pdf_file_path.split(".")[0]
+    output_file_path = f"{pdf_file_name}.png"
+
+    pdf_to_png(pdf_file_path, output_file_path)
+    print(f"PDF converted to PNG: {output_file_path}")
